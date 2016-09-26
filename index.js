98ba1639dd3fceefd86366c4928f815376d9ef46
Efthemi <ehatz01@gmail.com>
2016-09-25 22:26:08 -0400

    heroku deploy

--- index.js
+++ index.js
@@ -1,3 +1,64 @@
+
+// var projects = {
+// 	betFuture: {
+// 		herokuLink: 'https://betthefuture.herokuapp.com/',
+// 		githubLink: 'https://github.com/eHatz/Bet-The-Future',
+// 		details:
+// 		title: 'Bet The Future'
+// 	},
+// 	mongoArticle: {
+// 		herokuLink: 'https://ehatz-mongoarticle.herokuapp.com/',
+// 		githubLink: 'https://github.com/eHatz/week18-mongoCheerio',
+// 		details:
+// 		title: 'NYT Articles Scrape'
+// 	},
+// 	societyPin: {
+// 		herokuLink: 'https://societypin.herokuapp.com/',
+// 		githubLink: 'https://github.com/eHatz/societyPin',
+// 		details:
+// 		title: 'Society Pin'
+// 	},
+// 	animaltastic: {
+// 		herokuLink: 'https://ehatz-animaltastic.herokuapp.com/',
+// 		githubLink: 'https://github.com/eHatz/AnimalTastic',
+// 		details:
+// 		title: 'Animaltastic'
+// 	},
+// 	hangman: {
+// 		herokuLink: 'https://ehatz-hangman.herokuapp.com/',
+// 		githubLink: 'https://github.com/eHatz/week-3-game',
+// 		details:
+// 		title: 'Game of Thrones Hangman'
+// 	},
+// 	triviaGame: {
+// 		herokuLink: 'https://ehatz-triviagame.herokuapp.com/',
+// 		githubLink: 'https://github.com/eHatz/TriviaGame',
+// 		details:
+// 		title: 'Troll Trivia Game'
+// 	},
+// 	starwars: {
+// 		herokuLink: 'https://ehatz-starwars.herokuapp.com/',
+// 		githubLink: 'https://github.com/eHatz/week-4-game',
+// 		details:
+// 		title: 'Star Wars RPG'
+// 	},
+// 	bamazon: {
+// 		githubLink: 'https://github.com/eHatz/Bamazon',
+// 		details:
+// 		title: 'Bamazon'
+// 	},
+// 	rps: {
+// 		githubLink: 'https://github.com/eHatz/RPS',
+// 		details:
+// 		title: 'Rock Paper Scissors'
+// 	},
+// 	hangmanNode: {
+// 		githubLink: 'https://github.com/eHatz/Week11-Hangman'
+// 		details:
+// 		title: 'Node Hangman'
+// 	}
+// };
+
 $('.carousel-caption').on('click', function() {
-	console.log(this.id);
+	console.log(this.id)
 })
